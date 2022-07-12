9ec79faedf20a4d96812f478bd894f02aa6aff89
Tonyavall <tonyavallescas@gmail.com>
2022-07-11 19:55:57 -0700

    Updated index of models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,19 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Product.belongsTo(Category, {
-//   foreignKey
-// })
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
-// Categories have many Products
+Product.belongsToMany(Tag, { through: ProductTag })
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
