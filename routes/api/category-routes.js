4c46f1751862b75072a829d3138c6c5effe6e844
Tonyavall <tonyavallescas@gmail.com>
2022-07-12 23:13:17 -0700

    updated tag-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,11 +13,11 @@ router.get('/', async (req, res) => {
     })
 
     const allCategories = dbResponse.map(category => {
-      return category.get({plain: true})
+      return category.get({ plain: true })
     })
 
     res.status(200).json(allCategories)
-  } catch(err) {
+  } catch (err) {
     res.status(400).json(err)
   }
 });
@@ -27,9 +27,9 @@ router.get('/:id', async (req, res) => {
     const dbResponse = await Category.findByPk(req.params.id, {
       include: [{ all: true, nested: true }],
     })
-    const oneCategory = dbResponse.get({plain:true})
+    const oneCategory = dbResponse.get({ plain: true })
     res.status(200).json(oneCategory)
-  } catch(err) {
+  } catch (err) {
     res.status(400).json(err)
   }
 });
@@ -37,23 +37,22 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const dbResponse = await Category.create({
-      id: req.body.id,
       category_name: req.body.category_name,
     })
     res.status(200).json(dbResponse);
-  } catch(err) {
+  } catch (err) {
     res.status(400).json(err)
   }
 });
 
 router.put('/:id', async (req, res) => {
   try {
-    const dbResponse = await Category.update({
-      id: req.body.id,
-      category_name: req.body.category_name,
-    })
+    const dbResponse = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id } }
+    )
     res.status(200).json(dbResponse);
-  } catch(err) {
+  } catch (err) {
     res.status(400).json(err)
   }
 });
@@ -63,8 +62,11 @@ router.delete('/:id', async (req, res) => {
     const dbResponse = await Category.destroy({
       where: { id: req.params.id }
     })
-    res.status(200).json({message: 'Success', res: dbResponse})
-  } catch(err) {
+    res.status(200).json({
+      message: 'Success',
+      res: dbResponse
+    })
+  } catch (err) {
     res.status(400).json(err)
   }
 });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,26 +1,82 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const dbResponse = await Tag.findAll({
+      include: [{ all: true, nested: true }]
+    })
+    const allTags = dbResponse.map(tag => {
+      return tag.get({ plain: true })
+    })
+
+    res.status(200).json(allTags)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const dbResponse = await Tag.findByPk(req.params.id, {
+      include: [{ all: true, nested: true }]
+    })
+    const oneTag = dbResponse.get({ plain: true })
+
+    res.status(200).json(oneTag)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const dbResponse = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json({
+      message: 'success',
+      db: dbResponse
+    })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const dbResponse = await Tag.update(
+      { tag_name: req.body.tag_name },
+      { where: { id: req.params.id } }
+    )
+    res.status(200).json({
+      message: 'success',
+      db: dbResponse
+    })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const dbResponse = await Tag.destroy({
+      where: { id: req.params.id }
+    })
+    res.status(200).json({
+      message: 'success',
+      db: dbResponse
+    })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
